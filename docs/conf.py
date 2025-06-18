dd636cb794cc92474aff5a7ded364b6a2575cb6d
Somiyana <somiyanamir@gmail.com>
2025-06-18 16:38:08 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'How to Download LastPass on Mac'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
